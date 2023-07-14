75fc070ed4d32d0dace85018dc692ae269eb6f39
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-07-14 12:01:58 -0600

    Completed random fossil button.

--- src/random-fossil.js
+++ src/random-fossil.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+import axios from "axios";
+
+// Display a random fossil on button click:
+const randomButton = document.getElementById('get-random-fossil');
+randomButton.addEventListener('click', async (event) => {
+	// Await the fossil data:
+	let fossil = await axios.get('/random-fossil.json');
+	fossil = fossil.data;
+
+	// Display fossil image:
+	let image = document.getElementById('random-fossil-image');
+	image.innerHTML = `<img src="${fossil.img}" alt="${fossil.name}" />`;
+
+	// Display fossil name:
+	let name = document.getElementById('random-fossil-name');
+	name.innerText = fossil.name;
+});
